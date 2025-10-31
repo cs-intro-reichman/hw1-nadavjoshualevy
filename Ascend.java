0ed1e6b1d12fa457fd43def4c0d7edbc51fee34f
Nadav Joshua Levy <nadavjoshualevy@Nadavs-MacBook-Air.local>
2025-10-31 21:23:07 +0200

    code 4

--- Ascend.java
+++ Ascend.java
@@ -2,6 +2,27 @@
 // and prints them in increasing order.
 public class Ascend {
 	public static void main(String[] args) {
+
+		int range = Integer.parseInt(args[0]);
+		int r1 = (int) (Math.random() * range);
+		int r2 = (int) (Math.random() * range);
+		int r3 = (int) (Math.random() * range);
+
+		int a =  Math.min(r1,r2);
+		int min = Math.min(r3, a);
+		int c = Math.max(r1, r2);
+		int max = Math.max(c, r3);
+		int sum = r1 + r2 + r3 ;
+		int mid = sum - (max+min);
+
+
+		System.out.println(r1 + " " + r2 +" " + r3);
+		System.out.println(min + " " + mid +" " + max);
+		
+
+
+	
+
 		// Replace this comment with your code
 	}
 }
