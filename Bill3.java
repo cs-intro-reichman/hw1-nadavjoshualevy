0e7919a30ea9fd5373d87045ebc9e5a2c5ceba3a
Nadav Joshua Levy <nadavjoshualevy@Nadavs-MacBook-Air.local>
2025-10-31 18:01:44 +0200

    My First ever Code

--- Bill3.java
+++ Bill3.java
@@ -1,8 +1,14 @@
-// Splits a restaurant bill evenly among three diners.
+/// Splits a restaurant bill evenly among three diners.
 public class Bill3 {
 	public static void main(String[] args) {
-		// To get you started, here is the first line in the program:
-	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+		
+	    String name1 = args[0];//input 1
+		String name2 = args[1];//input 2
+		String name3 = args[2];//input 3
+	
+	    double bill = Double.parseDouble(args[3]);//input 4
+		double share = bill / 3;
+        double roudedShare = Math.ceil(share);
+	    System.out.println("Dear " + name3 + ", " + name2 + " "  + name1 + ": pay " + roudedShare + " Shekels each."); 
 	}
 }
